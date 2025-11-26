d72a6a9fd266c4b64c7deb0082e46e0d7bdebd3a
Ahmad Fathur <fathurahmad560@gmail.com>
2025-11-26 09:49:23 +0700

    perbaikan

--- database/migrations/2025_11_04_131306_create_kuis_table.php
+++ database/migrations/2025_11_04_131306_create_kuis_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('materi_id')->constrained('materis', 'materi_id');
             $table->string('judul');
             $table->text('deskripsi');
-            $table->text('slug')->unique();
+            $table->string('slug')->unique();
             $table->timestamps();
         });
     }
